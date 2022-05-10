a41870051cbb07aa11858023d8fba0ceae7908cf
daidr <daidr@daidr.me>
2022-05-10 22:01:01 +0800

    feat: ts support

---
+++ src/types.ts
@@ -0,0 +1,96 @@
+
+export type ServerTypes = 'cn' | 'os'
+export type serverRegions = 'cn_gf01' | 'cn_qd01' | 'os_usa' | 'os_euro' | 'os_asia' | 'os_cht'
+
+export interface IUserData {
+  current_resin: number
+  max_resin: number
+  resin_recovery_time: string
+  finished_task_num: number
+  total_task_num: number
+  is_extra_task_reward_received: boolean
+  remain_resin_discount_num: number
+  resin_discount_num_limit: number
+  current_expedition_num: number
+  max_expedition_num: number
+  expeditions: {
+    avatar_side_icon: string
+    status: string
+    remained_time: string
+  }[]
+  current_home_coin: number
+  max_home_coin: number
+  home_coin_recovery_time: string
+  transformer: {
+    obtained: boolean
+    recovery_time: {
+      Day: number
+      Hour: number
+      Minute: number
+      Second: number
+      reached: boolean
+    }
+  }
+}
+
+export interface IUserDataItem {
+  /**
+   * 是否启用（不启用则不显示，且不获取数据）
+   */
+  isEnabled: boolean
+  /**
+   * 服务器类型（国服/海外）
+   */
+  serverType: ServerTypes
+  /**
+   * 细分的服务器类型
+   */
+  serverRegion: serverRegions
+  /**
+   * 玩家id
+   */
+  uid: number
+  /**
+   * 玩家昵称
+   */
+  nickname: string
+  /**
+   * 用户cookie
+   */
+  cookie: string
+  /**
+   * 获取的数据
+   */
+  data: IUserData
+  /**
+   * 最后一次获取数据的时间
+   */
+  updateTimestamp: number
+  /**
+   * 是否出错
+   */
+  isError: boolean
+  /**
+   * 错误信息
+   */
+  errorMessage: string
+}
+
+export interface IRoleDataItem {
+  /**
+   * 玩家对应的服务器
+   */
+  region: serverRegions
+  /**
+   * 玩家uid
+   */
+  game_uid: number
+  /**
+   * 玩家昵称
+   */
+  nickname: string
+  /**
+   * 玩家等级
+   */
+  level: number
+}
