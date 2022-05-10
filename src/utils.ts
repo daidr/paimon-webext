ca0a8b982b5a2d28ba3617f0f05da43405f90c9c
daidr <daidr@daidr.me>
2022-05-10 17:01:03 +0800

    feat: refactor utils with ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,64 +1,105 @@
-function randomIntFromInterval(min, max) {
+function randomIntFromInterval(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function md5(string) {
-  function md5_RotateLeft(lValue, iShiftBits) {
+function md5(string: string) {
+  function md5_RotateLeft(lValue: number, iShiftBits: number) {
     return (lValue << iShiftBits) | (lValue >>> (32 - iShiftBits))
   }
-  function md5_AddUnsigned(lX, lY) {
-    const lX8 = (lX & 0x80000000)
-    const lY8 = (lY & 0x80000000)
-    const lX4 = (lX & 0x40000000)
-    const lY4 = (lY & 0x40000000)
+  function md5_AddUnsigned(lX: number, lY: number) {
+    const lX8 = lX & 0x80000000
+    const lY8 = lY & 0x80000000
+    const lX4 = lX & 0x40000000
+    const lY4 = lY & 0x40000000
     const lResult = (lX & 0x3FFFFFFF) + (lY & 0x3FFFFFFF)
-    if (lX4 & lY4)
-      return (lResult ^ 0x80000000 ^ lX8 ^ lY8)
+    if (lX4 & lY4) return lResult ^ 0x80000000 ^ lX8 ^ lY8
 
     if (lX4 | lY4) {
-      if (lResult & 0x40000000)
-        return (lResult ^ 0xC0000000 ^ lX8 ^ lY8)
-
-      else
-        return (lResult ^ 0x40000000 ^ lX8 ^ lY8)
+      if (lResult & 0x40000000) return lResult ^ 0xC0000000 ^ lX8 ^ lY8
+      else return lResult ^ 0x40000000 ^ lX8 ^ lY8
     }
     else {
-      return (lResult ^ lX8 ^ lY8)
+      return lResult ^ lX8 ^ lY8
     }
   }
-  function md5_F(x, y, z) {
-    return (x & y) | ((~x) & z)
+  function md5_F(x: number, y: number, z: number) {
+    return (x & y) | (~x & z)
   }
-  function md5_G(x, y, z) {
-    return (x & z) | (y & (~z))
+  function md5_G(x: number, y: number, z: number) {
+    return (x & z) | (y & ~z)
   }
-  function md5_H(x, y, z) {
-    return (x ^ y ^ z)
+  function md5_H(x: number, y: number, z: number) {
+    return x ^ y ^ z
   }
-  function md5_I(x, y, z) {
-    return (y ^ (x | (~z)))
+  function md5_I(x: number, y: number, z: number) {
+    return y ^ (x | ~z)
   }
-  function md5_FF(a, b, c, d, x, s, ac) {
-    a = md5_AddUnsigned(a, md5_AddUnsigned(md5_AddUnsigned(md5_F(b, c, d), x), ac))
+  function md5_FF(
+    a: number,
+    b: number,
+    c: number,
+    d: number,
+    x: number,
+    s: number,
+    ac: number,
+  ) {
+    a = md5_AddUnsigned(
+      a,
+      md5_AddUnsigned(md5_AddUnsigned(md5_F(b, c, d), x), ac),
+    )
     return md5_AddUnsigned(md5_RotateLeft(a, s), b)
   }
-  function md5_GG(a, b, c, d, x, s, ac) {
-    a = md5_AddUnsigned(a, md5_AddUnsigned(md5_AddUnsigned(md5_G(b, c, d), x), ac))
+  function md5_GG(
+    a: number,
+    b: number,
+    c: number,
+    d: number,
+    x: number,
+    s: number,
+    ac: number,
+  ) {
+    a = md5_AddUnsigned(
+      a,
+      md5_AddUnsigned(md5_AddUnsigned(md5_G(b, c, d), x), ac),
+    )
     return md5_AddUnsigned(md5_RotateLeft(a, s), b)
   }
-  function md5_HH(a, b, c, d, x, s, ac) {
-    a = md5_AddUnsigned(a, md5_AddUnsigned(md5_AddUnsigned(md5_H(b, c, d), x), ac))
+  function md5_HH(
+    a: number,
+    b: number,
+    c: number,
+    d: number,
+    x: number,
+    s: number,
+    ac: number,
+  ) {
+    a = md5_AddUnsigned(
+      a,
+      md5_AddUnsigned(md5_AddUnsigned(md5_H(b, c, d), x), ac),
+    )
     return md5_AddUnsigned(md5_RotateLeft(a, s), b)
   }
-  function md5_II(a, b, c, d, x, s, ac) {
-    a = md5_AddUnsigned(a, md5_AddUnsigned(md5_AddUnsigned(md5_I(b, c, d), x), ac))
+  function md5_II(
+    a: number,
+    b: number,
+    c: number,
+    d: number,
+    x: number,
+    s: number,
+    ac: number,
+  ) {
+    a = md5_AddUnsigned(
+      a,
+      md5_AddUnsigned(md5_AddUnsigned(md5_I(b, c, d), x), ac),
+    )
     return md5_AddUnsigned(md5_RotateLeft(a, s), b)
   }
-  function md5_ConvertToWordArray(string) {
+  function md5_ConvertToWordArray(string: string) {
     let lWordCount
     const lMessageLength = string.length
     const lNumberOfWords_temp1 = lMessageLength + 8
-    const lNumberOfWords_temp2 = (lNumberOfWords_temp1 - (lNumberOfWords_temp1 % 64)) / 64
+    const lNumberOfWords_temp2
+      = (lNumberOfWords_temp1 - (lNumberOfWords_temp1 % 64)) / 64
     const lNumberOfWords = (lNumberOfWords_temp2 + 1) * 16
     const lWordArray = Array(lNumberOfWords - 1)
     let lBytePosition = 0
@@ -66,7 +107,9 @@ function md5(string) {
     while (lByteCount < lMessageLength) {
       lWordCount = (lByteCount - (lByteCount % 4)) / 4
       lBytePosition = (lByteCount % 4) * 8
-      lWordArray[lWordCount] = (lWordArray[lWordCount] | (string.charCodeAt(lByteCount) << lBytePosition))
+      lWordArray[lWordCount]
+        = lWordArray[lWordCount]
+        | (string.charCodeAt(lByteCount) << lBytePosition)
       lByteCount++
     }
     lWordCount = (lByteCount - (lByteCount % 4)) / 4
@@ -76,16 +119,21 @@ function md5(string) {
     lWordArray[lNumberOfWords - 1] = lMessageLength >>> 29
     return lWordArray
   }
-  function md5_WordToHex(lValue) {
-    let WordToHexValue = ''; let WordToHexValue_temp = ''; let lByte; let lCount
+  function md5_WordToHex(lValue: number) {
+    let WordToHexValue = ''
+    let WordToHexValue_temp = ''
+    let lByte
+    let lCount
     for (lCount = 0; lCount <= 3; lCount++) {
       lByte = (lValue >>> (lCount * 8)) & 255
       WordToHexValue_temp = `0${lByte.toString(16)}`
-      WordToHexValue = WordToHexValue + WordToHexValue_temp.substr(WordToHexValue_temp.length - 2, 2)
+      WordToHexValue
+        = WordToHexValue
+        + WordToHexValue_temp.substr(WordToHexValue_temp.length - 2, 2)
     }
     return WordToHexValue
   }
-  function md5_Utf8Encode(string) {
+  function md5_Utf8Encode(string: string) {
     string = string.replace(/\r\n/g, '\n')
     let utftext = ''
     for (let n = 0; n < string.length; n++) {
@@ -93,7 +141,7 @@ function md5(string) {
       if (c < 128) {
         utftext += String.fromCharCode(c)
       }
-      else if ((c > 127) && (c < 2048)) {
+      else if (c > 127 && c < 2048) {
         utftext += String.fromCharCode((c >> 6) | 192)
         utftext += String.fromCharCode((c & 63) | 128)
       }
@@ -107,15 +155,33 @@ function md5(string) {
   }
   let x = []
   let k, AA, BB, CC, DD, a, b, c, d
-  const S11 = 7; const S12 = 12; const S13 = 17; const S14 = 22
-  const S21 = 5; const S22 = 9; const S23 = 14; const S24 = 20
-  const S31 = 4; const S32 = 11; const S33 = 16; const S34 = 23
-  const S41 = 6; const S42 = 10; const S43 = 15; const S44 = 21
+  const S11 = 7
+  const S12 = 12
+  const S13 = 17
+  const S14 = 22
+  const S21 = 5
+  const S22 = 9
+  const S23 = 14
+  const S24 = 20
+  const S31 = 4
+  const S32 = 11
+  const S33 = 16
+  const S34 = 23
+  const S41 = 6
+  const S42 = 10
+  const S43 = 15
+  const S44 = 21
   string = md5_Utf8Encode(string)
   x = md5_ConvertToWordArray(string)
-  a = 0x67452301; b = 0xEFCDAB89; c = 0x98BADCFE; d = 0x10325476
+  a = 0x67452301
+  b = 0xEFCDAB89
+  c = 0x98BADCFE
+  d = 0x10325476
   for (k = 0; k < x.length; k += 16) {
-    AA = a; BB = b; CC = c; DD = d
+    AA = a
+    BB = b
+    CC = c
+    DD = d
     a = md5_FF(a, b, c, d, x[k + 0], S11, 0xD76AA478)
     d = md5_FF(d, a, b, c, x[k + 1], S12, 0xE8C7B756)
     c = md5_FF(c, d, a, b, x[k + 2], S13, 0x242070DB)
@@ -185,17 +251,22 @@ function md5(string) {
     c = md5_AddUnsigned(c, CC)
     d = md5_AddUnsigned(d, DD)
   }
-  return (md5_WordToHex(a) + md5_WordToHex(b) + md5_WordToHex(c) + md5_WordToHex(d)).toLowerCase()
+  return (
+    md5_WordToHex(a)
+    + md5_WordToHex(b)
+    + md5_WordToHex(c)
+    + md5_WordToHex(d)
+  ).toLowerCase()
 }
 
-function getTime(time) {
+function getTime(time: number) {
   const hh = ~~(time / 3600)
   const mm = ~~((time % 3600) / 60)
 
   return `${hh}小时${mm}分钟`
 }
 
-function getClock(time) {
+function getClock(time: number) {
   const timeNow = Date.now()
   const now = new Date(timeNow)
   const hoursNow = now.getHours()
@@ -204,16 +275,68 @@ function getClock(time) {
   const timeRecovery = new Date(timeNow + time * 1000)
 
   const tillTommorow = (24 - hoursNow) * 3600 * 1000
-  const tommorow = timeNow + tillTommorow - minutesNow - secondsNow
+  const tommorow = new Date(timeNow + tillTommorow - minutesNow - secondsNow)
 
   let str = ''
-  if (timeRecovery < tommorow)
-    str = '今日'
-
-  else
-    str = '次日'
+  if (timeRecovery < tommorow) str = '今日'
+  else str = '次日'
 
   return ` ${str}, ${timeRecovery.getHours()}点${timeRecovery.getMinutes()}分`
 }
 
-export { md5, randomIntFromInterval, getTime, getClock }
+function stringifyParams(params: object) {
+  const keys = Object.keys(params)
+  const values = Object.values(params)
+  const paramsStr = keys.map((key, index) => `${key}=${values[index]}`).join('&')
+  return paramsStr
+}
+
+function getDS(oversea: boolean, params: object, body: object) {
+  const timestamp = Math.floor(Date.now() / 1000)
+  const randomStr = randomIntFromInterval(100000, 200000)
+  const bodyStr = (!body && Object.keys(body).length > 0) ? JSON.stringify(body) : ''
+  const paramStr = (!params && Object.keys(params).length > 0) ? stringifyParams(params) : ''
+  const salt = oversea ? 'okr4obncj8bw5a65hbnn5oo6ixjc3l9w' : 'xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs'
+  const text = `salt=${salt}&t=${timestamp}&r=${randomStr}&b=${bodyStr}&q=${paramStr}`
+  const sign = md5(text)
+  return `${timestamp},${randomStr},${sign}`
+}
+
+const HEADER_TEMPLATE_CN = {
+  'x-rpc-app_version': '2.23.1',
+  'User-Agent': 'Mozilla/5.0 (Linux; Android 12; Mi 10 Pro Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 miHoYoBBS/2.23.1',
+  'x-rpc-client_type': '5',
+  'Origin': 'https://webstatic.mihoyo.com',
+  'X-Requested-With': 'com.mihoyo.hyperion',
+  'Referer': 'https://webstatic.mihoyo.com/',
+}
+
+const HEADER_TEMPLATE_OS = {
+  'x-rpc-app_version': '2.9.0',
+  'User-Agent': 'Mozilla/5.0 (Linux; Android 12; Mi 10 Pro Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 miHoYoBBSOversea/2.9.0',
+  'x-rpc-client_type': '2',
+  'Origin': 'https://webstatic-sea.hoyolab.com',
+  'X-Requested-With': 'com.mihoyo.hoyolab',
+  'Referer': 'https://webstatic-sea.hoyolab.com',
+}
+
+function getHeader(oversea: boolean, params: object, body: object, ds: boolean) {
+  const client = oversea ? HEADER_TEMPLATE_OS : HEADER_TEMPLATE_CN
+  const header = new Headers({
+    'Accept': 'application/json, text/plain, */*',
+    'User-Agent': client['User-Agent'],
+    'X-Requested-With': client['X-Requested-With'],
+    'Origin': client.Origin,
+    'Referer': client.Referer,
+  })
+
+  if (ds) {
+    const dsStr = getDS(oversea, params, body)
+    header.append('DS', dsStr)
+    header.append('x-rpc-client_type', client['x-rpc-client_type'])
+    header.append('x-rpc-app_version', client['x-rpc-app_version'])
+  }
+  return header
+}
+
+export { md5, randomIntFromInterval, getTime, getClock, getDS, getHeader }
