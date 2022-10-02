6665151baeafe979899957410e0267028e40d330
daidr <daidr@daidr.me>
2022-10-02 18:55:23 +0800

    lint: import type

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,5 +1,5 @@
 import { useStorageLocal } from '~/composables/useStorageLocal'
-import { IUserDataItem } from '~/types'
+import type { IUserDataItem } from '~/types'
 
 export const storageRoleList = useStorageLocal<IUserDataItem[]>('hoyo_RoleList', [], { listenToStorageChanges: true })
 export const storageDefaultUid = useStorageLocal<number>('hoyo_DefaultUid', 0, { listenToStorageChanges: true })
